435babd97b94efa195fa1efc9711347c39a6cac1
Alan Stanley <astanley@upei.ca>
2014-11-23 23:33:24 -0400

    theme imnprovements

--- theme/dirt-dhcommons-block.tpl.php
+++ theme/dirt-dhcommons-block.tpl.php
@@ -1,9 +1,13 @@
 <?php ?>
 <div class ="dhcommons_results_block">
+  <p class ="dh_commons_intro">
+    <?php print $intro; ?>
+  </p>
+
   <ul class ="project_links">
-   <?php foreach($variables['project_links'] as $project_link): ?>
-    <li class ="project_link"><?php print $project_link; ?></li>
+    <?php foreach ($variables['project_links'] as $project_link): ?>
+      <li class ="project_link"><?php print $project_link; ?></li>
     <?php endforeach; ?>
-    </ul>
+  </ul>
   <div class ="dhcommons_message"><?php print $overflow; ?></div>
 </div>
